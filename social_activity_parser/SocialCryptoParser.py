2afe4a8a12f8007d19fd506f7089d827285b5434
usrsem <user7em@gmail.com>
2021-12-11 23:17:05 +0300

    Adding interface for social info parsers

--- social_activity_parser/SocialCryptoParser.py
+++ social_activity_parser/SocialCryptoParser.py
@@ -1,5 +1,8 @@
+from pandas.core.frame import DataFrame
+from social_activity_parser.twircy.ParserRequest import ParserRequest
+
 
 class SocialCryptoParser:
 
-	def parse_data(self):
-		raise UnimplementedError
+	def parse_data(self, req: ParserRequest) -> DataFrame:
+		raise NotImplementedError
