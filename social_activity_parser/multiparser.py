d9e6f77a4bf8d9888ec0147b9aa481f024d7798e
usrsem <user7em@gmail.com>
2021-12-11 23:21:36 +0300

    Adding test multiparser

--- social_activity_parser/multiparser.py
+++ social_activity_parser/multiparser.py
@@ -4,10 +4,27 @@ import apis_list
 import pandas as pd
 from pandas import DataFrame
 from requests_html import HTMLSession
-from twircy import TwitterParser
+from twircy import twitter_parser as tp
 import json
 from loguru import logger
 
+
+def get_tweets(search_phrase: str, days: int = 5, limit: int = 50) -> DataFrame:
+
+	coins: list[str] = send_get_request()
+
+    items = []
+    df: DataFrame
+
+    df = tp.search()
+
+    if len(df.index) > 0:
+        df['coin'] = search_phrase
+        items.append(df)
+    logger.debug(df)
+
+    df = pd.concat(items)
+    return df
 
 
 def send_get_request(url: str) -> Any:
@@ -17,47 +34,14 @@ def send_get_request(url: str) -> Any:
     return result
 
 
-def convert_coins_to_twitter_search_request_url(coins: tuple[str]) -> str:
-	search_req = ""
-	req_start: str = 'https://twitter.com/search?q='
-	req_end: str = '&src=typed_query'
-
-	for i in coins:
-		search_req += f'{i}%20OR%20'
-
-	return req_start + search_req + req_end
-
-
-def get_tweets(search_phrase: str, days: int = 5, limit: int = 50) -> DataFrame:
-	items = []
-	df: DataFrame
-	parser: TwitterParser = TwitterParser(
-		limit=int(limit),
-		search_phrase=search_phrase,
-		for_days=int(days)
-	)
-
-	df = parser.search()
-
-	if len(df.index) > 0:
-		df['coin'] = search_phrase
-		items.append(df)
-	logger.debug(df)
-
-	df = pd.concat(items)
-	return df
-
-
-
-
 if __name__ == "__main__":
-	coins = send_get_request(apis_list.coinlist_url)['Data']
-	search = convert_coins_to_twitter_search_request_url(coins[0:10])
-	df = get_tweets('EKB')
+    coins = send_get_request(apis_list.coinlist_url)['Data']
+    search = convert_coins_to_twitter_search_request_url(coins[0:10])
+    df = get_tweets('EKB')
 
-	df.reset_index(drop=True, inplace=True)
-	print(df)
+    df.reset_index(drop=True, inplace=True)
+    print(df)
 
-	print(len(df))
-	print(df.columns)
-	logger.debug("That's it, beautiful and simple logging!")
+    print(len(df))
+    print(df.columns)
+    logger.debug("That's it, beautiful and simple logging!")
