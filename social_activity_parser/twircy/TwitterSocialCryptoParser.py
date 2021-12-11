ecc71e06d347e63d550bb736edb07df8f8217a23
usrsem <user7em@gmail.com>
2021-12-11 23:17:20 +0300

    Implementing social parser for twitter

--- social_activity_parser/twircy/TwitterSocialCryptoParser.py
+++ social_activity_parser/twircy/TwitterSocialCryptoParser.py
@@ -6,16 +6,17 @@ from loguru import logger as log
 from stweet.model.tweet import Tweet
 from stweet.search_runner.search_tweets_task import SearchTweetsTask
 from stweet.tweet_output.collector_tweet_output import CollectorTweetOutput
+from social_activity_parser.SocialCryptoParser import SocialCryptoParser
+from social_activity_parser.twircy.ParserRequest import ParserRequest
 
-from TwitterRequest import TwitterRequest
 
-
-class TwitterParser:
-
-	def __init__(self):
+class TwitterSocialCryptoParser(SocialCryptoParser):
+	def __init__(self) -> None:
+		super().__init__()
 		self.tweets_collector: CollectorTweetOutput = st.CollectorTweetOutput()
 
-	def search(self, req: TwitterRequest) -> DataFrame:
+	def parse_data(self, req: ParserRequest) -> DataFrame:
+		"""Gets tweets by stweet as DataFrame"""
 
 		search_tweets_task: SearchTweetsTask = st.SearchTweetsTask(
 			all_words=req.search_phrase,
@@ -24,26 +25,20 @@ class TwitterParser:
 		)
 
 		st.TweetSearchRunner(
-		    search_tweets_task=search_tweets_task,
-		    tweet_outputs=[self.tweets_collector]
+			search_tweets_task=search_tweets_task,
+			tweet_outputs=[self.tweets_collector]
 		).run()
 
 		output: list[Tweet] = self.tweets_collector.get_scrapped_tweets()
-		log.debug(f'Total Tweets collected: {len(output)}')
 
 		df: DataFrame = self._tweets_to_df(output)
 
-		df.to_excel(f'{req.search_phrase}.xlsx')
-		log.debug(f'Tweets are saved to "{req.search_phrase}.csv"')
+		log.debug(f'Total Tweets collected: {len(output)}')
 
 		return df
 
 	def _tweets_to_df(self, tweets: list[Tweet]) -> DataFrame:
 		df = pd.DataFrame(tweets)
-
 		# TODO: Convert str lambda to correct datetime type
 		df['created_at'] = df['created_at'].apply(lambda a: str(a))
-
 		return df
-
-
