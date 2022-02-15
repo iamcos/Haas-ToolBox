da495cba468a5e92ed138ac9e952808270aa34c6
usrsem <andrey7menov@gmail.com>
2022-02-15 06:01:43 +0300

    Improving doc

--- cli/bots/BotCli.py
+++ cli/bots/BotCli.py
@@ -1,10 +1,9 @@
 class BotCli:
     """
-    Base interface for bot setting cli
+    Base interface for bot setting CLI
     """
     def menu(self) -> None:
         """
-        To implement method for showing main menu
-        for current Bot
+        To implement method for showing main menu for current Bot
         """
         raise NotImplementedError("Method must be implemented")
