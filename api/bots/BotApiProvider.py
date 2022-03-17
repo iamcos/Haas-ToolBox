2dbb07fbf13e54657aa74df1b0c950d4dcb1e6b7
idevtier <idevtier@gmail.com>
2022-03-17 03:06:52 +0300

    Adding interface for provider pattern

---
+++ api/bots/BotApiProvider.py
@@ -0,0 +1,44 @@
+from abc import abstractmethod
+from typing import Any, Callable
+from haasomeapi.dataobjects.custombots.MadHatterBot import MadHatterBot
+from haasomeapi.dataobjects.custombots.dataobjects.Indicator import Indicator
+from haasomeapi.dataobjects.custombots.dataobjects.Insurance import Insurance
+from haasomeapi.dataobjects.custombots.dataobjects.Safety import Safety
+from haasomeapi.dataobjects.tradebot.TradeBot import TradeBot
+from haasomeapi.dataobjects.util.HaasomeClientResponse import HaasomeClientResponse
+
+
+# TODO: Add all bots types
+Bot = TradeBot | MadHatterBot
+Interfaces = Indicator | Safety | Insurance
+
+class BotApiProvider():
+    @abstractmethod
+    def get_all_bots(self) -> tuple[Bot]: pass
+
+    @abstractmethod
+    def get_all_interfaces(self, guid: str) -> tuple[Interfaces]: pass
+
+    @abstractmethod
+    def get_interfaces_by_type(
+        self,
+        guid: str,
+        t: Interfaces
+    ) -> tuple[Interfaces]: pass
+
+    @abstractmethod
+    def get_refreshed_bot(self, guid: str) -> Bot: pass
+
+    @abstractmethod
+    def get_edit_interface_method(self, t: Interfaces) -> Callable: pass
+
+    @abstractmethod
+    def get_backtest_method(self) -> Callable: pass
+
+    @abstractmethod
+    def process_error(
+        self,
+        response: HaasomeClientResponse,
+        message: str
+    ) -> Any: pass
+
