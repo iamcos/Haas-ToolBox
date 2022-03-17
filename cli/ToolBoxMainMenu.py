f15e31fd9f9ac8517e757e55b64538a33300d2b6
idevtier <idevtier@gmail.com>
2022-03-17 03:06:11 +0300

    Adding all bots to promt

--- cli/ToolBoxMainMenu.py
+++ cli/ToolBoxMainMenu.py
@@ -4,11 +4,6 @@ from typing import Any
 from InquirerPy import inquirer
 from InquirerPy.prompts.list import ListPrompt
 from cli.bots.BotCli import BotCli
-# from api.bots.flash_crash import FlashCrashBotManager
-# from api.bots.interactive.InteractiveBotManager import InteractiveBotManager
-# from api.bots.mad_hatter.MadHatterBotManager import MadHatterBotManager
-# from api.TradingViewManager import TradingViewManager
-# from api.bots.scalper.ScalperBotManager import ScalperBotManager
 from cli.bots.tradebot.TradeBotCli import TradeBotCli
 from cli.bots.madhatter.MadHatterCli import MadHatterCli
 
@@ -19,18 +14,17 @@ class ToolBoxMainMenu:
     """
 
     def __init__(self) -> None:
-        # TODO: Use CLI classes here, not managers !
         self._start_message: str = "Choose action: "
-        self._bot_chain: dict[str, type[BotCli]] = {
+        self._bot_chain: dict[str, type[BotCli] | None] = {
             # Fully configurable
             "Trade Bots": TradeBotCli,
             "Mad-Hatter Bots": MadHatterCli,
             # Custom
-            # "[Not working] Mad-Hatter Bots": None,
-            # "[Not working] Scalper Bots": None,
-            # "[Not working] Flash-Crash Bots": None,
-            # "[Not working] AssistedBT": None,
-            # "[Not working] TradingView": None,
+            "[Not working] Mad-Hatter Bots": None,
+            "[Not working] Scalper Bots": None,
+            "[Not working] Flash-Crash Bots": None,
+            "[Not working] AssistedBT": None,
+            "[Not working] TradingView": None,
             # Special class with keyboard interrupt
             "Quit": QuitOption
         }
