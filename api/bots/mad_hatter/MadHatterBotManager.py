8baba8816008e415519837bb6e1f9f6610e47316
idevtier <idevtier@gmail.com>
2022-03-17 03:07:52 +0300

    Adding provider to mad hatter manager

--- api/bots/mad_hatter/MadHatterBotManager.py
+++ api/bots/mad_hatter/MadHatterBotManager.py
@@ -1,241 +1,53 @@
-from collections import defaultdict
-import json
-from typing import Any
-from haasomeapi.dataobjects.custombots.BaseCustomBot import BaseCustomBot
-
-import pandas as pd
+from typing import Type
+from haasomeapi.dataobjects.custombots.MadHatterBot import MadHatterBot
 from haasomeapi.dataobjects.util.HaasomeClientResponse import HaasomeClientResponse
-from haasomeapi.enums.EnumMadHatterIndicators import EnumMadHatterIndicators
-from haasomeapi.enums.EnumMadHatterSafeties import EnumMadHatterSafeties
-from pandas.core.frame import DataFrame
-from ratelimit import limits, sleep_and_retry
-from sqlalchemy.util.deprecations import deprecated
-from api.scripts.config_manager import ConfigManager
+from api.bots.BacktestsCache import BacktestsCache, BotRoiData
+from api.bots.BotWrapper import BotWrapper
 
-import madhatter_consts
-from api.Haas import Haas
-from market_data import MarketData
-
-from api.MainContext import main_context
+from api.bots.BotManager import BotManager, Interfaces
+from api.bots.mad_hatter.MadHatterApiProvider import MadHatterApiProvider
 
 
-# TODO: Go to composition
-class MadHatterBotManager:
-
+class MadHatterBotManager(BotManager):
     def __init__(self):
-        self.haas: Haas = main_context.haas
-        self.stoploss_range: list[float] = []
-        self.num_configs: int
-        self.limit: int
-        self.bot_mode: int
-        self.roi_threshold: float = 0.2
-        self.config_storage: dict = {}
-        self.configs: dict = {}
-        self.current_config = None
-        self.extended_range = None
-        self.selected_intervals: Any = None
-        self.intervals: list[int] = madhatter_consts.intervals
-        self.columns: list[str] = madhatter_consts.columns
-        self.possible_profit: int
-        self.config_manager: ConfigManager = main_context.config_manager
-        self.ticks: int = self.config_manager.read_ticks()
+        self._wbot: BotWrapper = BotWrapper()
+        self.provider = MadHatterApiProvider()
+        self.backtests_cache = BacktestsCache()
 
-    @sleep_and_retry
-    @limits(calls=5, period=1)
-    def get_mad_hatter_bots(self) -> list[BaseCustomBot]:
-        custom_bots = self.haas.client.customBotApi.get_all_custom_bots()
-        return [x for x in custom_bots.result if x.botType == 15]
+    def bot_not_selected(self) -> bool:
+        return self._wbot.bot_is_not_set()
 
-    def get_bot_config_as_dataframe(self, bot: BaseCustomBot) -> DataFrame:
-        bot_dict = madhatter_consts.get_bot_dict(bot)
-        df = pd.DataFrame(bot_dict, index=[0])
-        return df
+    def set_bot(self, bot: MadHatterBot) -> None:
+        print(f"Selectd bot: {bot}")
+        self._wbot.bot = bot
 
-    def setup_bot_from_df(self, bot: Any, config: DataFrame) -> HaasomeClientResponse[BaseCustomBot]:
-        self._set_safeties(bot)
-        self._set_indicators(bot, config)
+    def get_available_bots(self) -> tuple[MadHatterBot]:
+        return self.provider.get_all_bots()
 
-        return self.haas.client.customBotApi.setup_mad_hatter_bot(
-            botName=bot.name,
-            botGuid=bot.guid,
-            accountGuid=bot.accountId,
-            primaryCoin=bot.priceMarket.primaryCurrency,
-            secondaryCoin=bot.priceMarket.secondaryCurrency,
-            contractName=bot.priceMarket.contractName,
-            leverage=bot.leverage,
-            templateGuid=bot.customTemplate,
-            position=bot.coinPosition,
-            fee=bot.currentFeePercentage,
-            tradeAmountType=bot.amountType,
-            tradeAmount=bot.currentTradeAmount,
-            useconsensus=bot.useTwoSignals,
-            disableAfterStopLoss=bot.disableAfterStopLoss,
-            interval=config.interval,
-            includeIncompleteInterval=bot.includeIncompleteInterval,
-            mappedBuySignal=bot.mappedBuySignal,
-            mappedSellSignal=bot.mappedSellSignal,
+    def get_interfaces_by_type(self, t: Type[Interfaces]) -> tuple[Interfaces]:
+        return self.provider.get_interfaces_by_type(self._wbot.guid, t)
+
+    # TODO: Implement edit interface method
+    def refresh_bot(self) -> None:
+        self._wbot.bot = self.provider.get_refreshed_bot(self._wbot.bot.guid)
+
+    def backtest_bot(self, interval: int):
+        res: HaasomeClientResponse = self.provider.get_backtest_method()(
+            self._wbot.guid, interval
         )
 
-    def _set_safeties(self, bot: Any) -> None:
-        safeties_config: dict[EnumMadHatterSafeties,
-                              int] = self._generate_safeties_config()
+        self.provider.process_error(
+            res, "Error while backtesting bot")
 
-        for current_type, value in safeties_config.items():
-            self.haas.client.customBotApi.set_mad_hatter_safety_parameter(
-                bot.guid, current_type, value
-            )
+    def save_roi(self, data: BotRoiData) -> None:
+        roi: float = self.bot_roi()
+        if roi > 0:
+            self.backtests_cache.add_data(data)
 
-    def _set_indicators(self, bot: Any, config: DataFrame) -> None:
-        indicators_config: dict[
-            EnumMadHatterIndicators, list[dict[int, str or int or bool]]] = self._generate_indicators_config(config)
+    def bot_roi(self) -> float:
+        self.refresh_bot()
+        return self._wbot.roi
 
-        for current_type, values in indicators_config.items():
-            for fieldNo, value in values:
-                self.haas.client.customBotApi.set_mad_hatter_indicator_parameter(
-                    bot.guid, current_type, fieldNo, value
-                )
-
-    def _generate_safeties_config(
-        self
-    ) -> defaultdict[EnumMadHatterSafeties, int]:
-        return defaultdict(lambda: 0)
-
-    def _generate_indicators_config(
-        self,
-        config: DataFrame
-    ) -> dict[EnumMadHatterIndicators, list[dict[int, str | int | bool]]]:
-
-        return {
-            EnumMadHatterIndicators.BBANDS: [
-                {0: int(config['bbl'])},
-                {1: config['devup']},
-                {2: config['devdn']},
-                {3: config['mattype']},
-                {5: bool(config['fcc'])},
-                {6: bool(config['resetmiddle'])},
-                {7: bool(config['allowmidsells'])}
-            ],
-            EnumMadHatterIndicators.RSI: [
-                {0: config['rsil']},
-                {1: config['rsib']},
-                {2: config['rsis']}
-            ],
-            EnumMadHatterIndicators.MACD: []
-        }
-
-    def create_top_bots(self):
-        bot = self.bot
-        configs = self.config_storage[bot.guid]
-
-        print(f"{configs=}")
-
-        if self.limit > len(configs.index):
-            self.limit = len(configs.index)
-
-        print(f"{self.limit=}")
-
-        for c in range(self.limit):
-            name = f"{self.bot.priceMarket.primaryCurrency}/"
-            f"{self.bot.priceMarket.secondaryCurrency} {c} {configs.roi.iloc[c]}% "
-
-            self.setup_bot_from_df(bot, configs.iloc[c])
-            self.haas.client.customBotApi.backtest_custom_bot(
-                bot.guid, self.config_manager.read_ticks())
-            self.haas.client.customBotApi.clone_custom_bot_simple(
-                bot.accountId, bot.guid, name)
-
-    def prepare_configs(self, configs: DataFrame) -> DataFrame:
-        configs.loc[0:-1, "obj"] = None
-        configs.loc[0:-1, "roi"] = 0
-
-        # FIXME: Do without loop
-        for c in configs.columns:
-            if c not in madhatter_consts.cols:
-                configs.drop(c, axis=1, inplace=True)
-
-        return configs
-
-    def check_bot_trade_amount(self, bot):
-        bt = self.haas.client.customBotApi.backtest_custom_bot(
-            bot.guid, self.ticks
-        ).result
-
-        return self.set_min_trade_amount(bt)
-
-    def set_min_trade_amount(self, bot):
-        for x in bot.botLogBook:
-            if "Minimum trade amount: " in x:
-                a = x.partition("Minimum trade amount: ")
-                b = a[2].partition(". Amount decimals")
-                min_trade_amount = float(b[0])
-                bot.currentTradeAmount = min_trade_amount
-                return bot
-            else:
-                return bot
-
-    def read_limits(self):
-        prefix: str = '_cfg_'
-        cfg_funcs_chain: list[str] = [
-            entry[0] for entry in MadHatterBotManager.__dict__.items() if prefix in entry[0]]
-
-        for func_name in cfg_funcs_chain:
-            cfg_fun = getattr(self, func_name)
-            prop_name = func_name.replace(prefix, '')
-            self.__dict__[prop_name] = cfg_fun()
-
-    def get_market_data(self):
-        market_data = MarketData().get_market_data(
-            self.bot.priceMarket, self.bot.interval, int(
-                self.ticks) / self.bot.interval
-        )
-        return market_data
-
-    def _cfg_num_configs(self) -> int:
-        return int(self.haas.config_parser["MH_LIMITS"].get("number_of_configs_to_apply"))
-
-    def _cfg_roi_threshold(self) -> float:
-        return float(self.haas.config_parser["MH_LIMITS"].get("set_acceptable_roi_threshold"))
-
-    def _cfg_bot_mode(self) -> int:
-        return int(self.haas.config_parser["MH_LIMITS"].get("set_backtesting_mode"))
-
-    def _cfg_limit(self) -> int:
-        return int(self.haas.config_parser["MH_LIMITS"].get("limit_to_create"))
-
-    def _cfg_stoploss_range(self) -> list[float]:
-        return list([
-            float(self.haas.config_parser["MH_LIMITS"].get(
-                "stoploss_range_start")),
-            float(self.haas.config_parser["MH_LIMITS"].get(
-                "stoploss_range_stop")),
-            float(self.haas.config_parser["MH_LIMITS"].get(
-                "stoploss_range_step")),
-        ])
-
-    def _cfg_selected_intervals(self) -> Any:
-        return json.loads(self.haas.config_parser["MH_LIMITS"].get("selected_intervals"))
-
-    # TODO: Using only in test, remove
-    @deprecated
-    def create_mad_hatter_bot(self, template_bot, bot_name) -> Any:
-        """[summary]
-
-        Args: template_bot : [One bot that will be used as a template for newly created bot. New bot will have the
-        same account id, bot type, primary/secondary currency, contract] bot_name : [newly created bot name]
-
-        Returns:
-                [type]: [description]
-        """
-        return self.haas.client.customBotApi.new_custom_bot(
-            template_bot.accountId,
-            template_bot.botType,
-            bot_name,
-            template_bot.priceMarket.primaryCurrency,
-            template_bot.priceMarket.secondaryCurrency,
-            template_bot.priceMarket.contractName,
-        ).result
-
-
-if __name__ == "__main__":
-    mh = MadHatterBotManager().menu()
+    def bot_name(self) -> str:
+        return "Mad Hatter Bot"
 
