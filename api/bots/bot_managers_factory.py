bd43fa56dbce3405fa0925c974cda74bbfc7a43d
idevtier <idevtier@gmail.com>
2022-03-17 03:09:17 +0300

    Adding factory pattern for creating bot managers

---
+++ api/bots/bot_managers_factory.py
@@ -0,0 +1,23 @@
+from typing import Type
+from haasomeapi.dataobjects.custombots.MadHatterBot import MadHatterBot
+
+from haasomeapi.dataobjects.tradebot.TradeBot import TradeBot
+
+from api.bots.mad_hatter.MadHatterBotManager import MadHatterBotManager
+from api.bots.trade.TradeBotManager import TradeBotManager
+
+
+class BotManagerCreationException(Exception):
+    pass
+
+
+def get_bot_manager(t: Type):
+    if t is TradeBot:
+        return TradeBotManager()
+    elif t is MadHatterBot:
+        return MadHatterBotManager()
+    else:
+        raise BotManagerCreationException(
+            f"Passed type {t} is wrong or not implemented"
+        )
+
