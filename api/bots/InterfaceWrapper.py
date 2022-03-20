c659b5174e0b5966f416b6ca5b64e657d9dbdff9
idevtier <idevtier@pop-os.localdomain>
2022-03-20 04:25:12 +0300

    Refactoring interface wrapper class

--- api/bots/InterfaceWrapper.py
+++ api/bots/InterfaceWrapper.py
@@ -5,7 +5,7 @@ from haasomeapi.dataobjects.custombots.dataobjects.Safety import Safety
 from api.bots.BotManager import Interfaces
 
 
-class BoostedInterface:
+class InterfaceWrapper:
     def __init__(self, interface: Interfaces) -> None:
         self.interface: Interfaces = interface
 
@@ -32,6 +32,11 @@ class BoostedInterface:
                 return self.interface.indicatorName
         raise BoostedInterfaceException(
             f"Passes not an interface: {self.interface}")
+
+    @property
+    def guid(self) -> str:
+        return self.interface.guid
+
 
 class BoostedInterfaceException(Exception):
     pass
---
+++ api/model/models.py
@@ -0,0 +1,9 @@
+from typing import NamedTuple
+
+
+class UsedOptionParameters(NamedTuple):
+    roi: float
+    ticks: int
+    step: str
+    parameter_value: str
+
