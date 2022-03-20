a0dc317b61cc1f1fff3c7273facf5a2e6199dd72
idevtier <idevtier@pop-os.localdomain>
2022-03-20 04:25:40 +0300

    Adding method for getting best result of backtesting

--- api/bots/BacktestsCache.py
+++ api/bots/BacktestsCache.py
@@ -1,5 +1,8 @@
+from functools import cmp_to_key
 from typing import DefaultDict, NamedTuple
-from api.bots.BotManager import InterfaceGuid
+
+
+InterfaceGuid = str
 
 
 class BotRoiData(NamedTuple):
@@ -17,4 +20,23 @@ class BacktestsCache:
 
     def add_data(self, data: BotRoiData) -> None:
         self.roi_values[data.intreface_guid].add(data)
+
+    def get_best_result(
+        self,
+        interface_guid: InterfaceGuid,
+        option_num: int
+    ) -> BotRoiData:
+
+        values = [
+            i
+            for i in self.roi_values[interface_guid]
+            if i.option_num == option_num
+        ]
+
+        return max(
+            values,
+            key=cmp_to_key(
+                lambda a, b: 1 if a.roi > b.roi else -1
+            )
+        )
 
