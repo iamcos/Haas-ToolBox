353ada9ba234ece4ac761c0fb102759869f8fa11
usrsem <andrey7menov@gmail.com>
2022-03-25 21:08:11 +0300

    Refactoring models module

---
+++ api/models.py
@@ -0,0 +1,22 @@
+from typing import NamedTuple
+from haasomeapi.dataobjects.custombots.MadHatterBot import MadHatterBot
+from haasomeapi.dataobjects.custombots.dataobjects.Indicator import Indicator
+from haasomeapi.dataobjects.custombots.dataobjects.Insurance import Insurance
+from haasomeapi.dataobjects.custombots.dataobjects.Safety import Safety
+from haasomeapi.dataobjects.tradebot.TradeBot import TradeBot
+
+
+class UsedOptionParameters(NamedTuple):
+    roi: float
+    ticks: int
+    step: str
+    parameter_value: str
+
+
+InterfaceGuid = str
+
+# TODO: Add all bots types
+Bot = TradeBot | MadHatterBot
+
+Interfaces = Indicator | Safety | Insurance
+
