f7e6a99a6f3951513e2a9a0f3323753a48f5a753
usrsem <andrey7menov@gmail.com>
2022-03-25 21:08:59 +0300

    Refactoring provider factory

--- api/bots/bot_managers_factory.py
+++ api/bots/bot_managers_factory.py
@@ -2,18 +2,13 @@ from typing import Type
 from haasomeapi.dataobjects.custombots.MadHatterBot import MadHatterBot
 
 from haasomeapi.dataobjects.tradebot.TradeBot import TradeBot
-from api.bots.BotApiProvider import Bot, BotApiProvider
 from api.bots.BotManager import BotManager
 
 from api.bots.mad_hatter.MadHatterBotManager import MadHatterBotManager
-from api.bots.trade.TradeBotApiProvider import TradeBotApiProvider
-from api.bots.mad_hatter.MadHatterApiProvider import MadHatterApiProvider
 from api.bots.trade.TradeBotManager import TradeBotManager
 
 
 class BotManagerCreationException(Exception): pass
-
-class BotApiProviderCreationException(Exception): pass
 
 
 def get_bot_manager(t: Type) -> BotManager:
@@ -27,12 +22,3 @@ def get_bot_manager(t: Type) -> BotManager:
             f"Passed type {t} is wrong or not implemented"
         )
 
-def get_provider(t: Type[Bot]) -> BotApiProvider:
-    if t is TradeBot:
-        return TradeBotApiProvider()
-    elif t is MadHatterBot:
-        return MadHatterApiProvider()
-    else:
-        raise BotApiProviderCreationException(
-            f"Passed type {t} is wrong or not implemented"
-        )
