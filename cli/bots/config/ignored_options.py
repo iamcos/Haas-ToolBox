1ac4cf653abe9c6a2c9e366605f720a4025901bd
usrsem <andrey7menov@gmail.com>
2022-03-25 21:13:52 +0300

    Fixing Mad Hatter Bot name

--- cli/bots/config/ignored_options.py
+++ cli/bots/config/ignored_options.py
@@ -1,12 +1,10 @@
 """Config for ignoring some options in CLI selection promt"""
-
-
 from typing import DefaultDict
 
 
 ignored_options: DefaultDict[str, DefaultDict[str, tuple[str, ...]]] = \
     DefaultDict(lambda: DefaultDict(lambda: ()))
 
-ignored_options["Mad Hatter Bot"]["Mad Hatter RSI"] = tuple(["Length"])
-ignored_options["Mad Hatter Bot"]["Mad Hatter BBands"] = tuple(["Length"])
+ignored_options["MadHatterBot"]["Mad Hatter RSI"] = tuple(["Length"])
+ignored_options["MadHatterBot"]["Mad Hatter BBands"] = tuple(["Length"])
 
