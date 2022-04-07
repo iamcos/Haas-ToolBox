ddce0c708d8d7dc97d9f2243e4349a3dee2795ca
usrsem <andrey7menov@gmail.com>
2022-04-08 01:27:24 +0300

    Renaming

--- cli/bots/scalper/ScalperCli.py
+++ cli/bots/scalper/ScalperCli.py
@@ -2,7 +2,7 @@ from haasomeapi.dataobjects.custombots.ScalperBot import ScalperBot
 from cli.bots.BotCli import BotCli
 
 
-class SclaperCli(BotCli):
+class ScalperCli(BotCli):
     def __init__(self) -> None:
         super().__init__(ScalperBot)
 
