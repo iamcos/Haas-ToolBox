f1720f66bf469696f036eb9551e60acdaa186068
usrsem <andrey7menov@gmail.com>
2022-04-15 10:05:14 +0300

    Adding separated class for inputting data

---
+++ api/scripts/inquirer_wrappers.py
@@ -0,0 +1,29 @@
+from InquirerPy import inquirer
+from loguru import logger as log
+
+
+def input_float(message: str, default: float) -> float:
+    result: str =inquirer.text(
+        message=message,
+        default=str(default)
+    ).execute()
+
+    try:
+        return float(result)
+    except ValueError:
+        log.warning("Input a number")
+        return input_float(message, default)
+
+
+def input_int(message: str, default: int) -> int:
+    result: str =inquirer.text(
+        message=message,
+        default=str(default)
+    ).execute()
+
+    try:
+        return int(result)
+    except ValueError:
+        log.warning("Input a number")
+        return input_int(message, default)
+
