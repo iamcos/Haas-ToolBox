2aa3c49bd76f3a0b50601a702001b0145d3a12a4
usrsem <andrey7menov@gmail.com>
2022-04-18 21:37:59 +0300

    Refactoring

--- api/bots/scalper/ScalperRangeBacktesterApi.py
+++ api/bots/scalper/ScalperRangeBacktesterApi.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+from haasomeapi.dataobjects.custombots.dataobjects.Safety import Safety
 from api.bots.scalper.ScalperBotManager import ScalperBotManager
 from api.MainContext import main_context
 from api.models import SclaperBacktestSample
@@ -12,7 +15,8 @@ class ScalperRangeBacktesterApi:
 
     def __init__(self, manager: ScalperBotManager) -> None:
         self.manager: ScalperBotManager = manager
-        self.cache: dict[ROI, tuple[float, float]] = {}
+        self.cache: defaultdict[ROI, list[tuple[float, float]]] = \
+                defaultdict(list)
         self.ticks = main_context.config_manager.read_ticks()
 
     def backtest(
@@ -21,19 +25,22 @@ class ScalperRangeBacktesterApi:
             top_bots_count: int
         ) -> None:
 
-        for (target_percentage, stop_loss) in self.perm_generator(sample):
-            log.info(f"{target_percentage=}, {stop_loss=}")
+        with self.manager.new_bot():
+            for (target_percentage, stop_loss) in self.perm_generator(sample):
+                log.info(f"{target_percentage=}, {stop_loss=}")
 
-            self.manager.edit_interface(Indicator(), 1, target_percentage)
-            self.manager.edit_interface(Indicator(), 2, stop_loss)
+                self.manager.edit_interface(Indicator(), 1, target_percentage)
+                self.manager.edit_interface(Safety(), 2, stop_loss)
 
-            self.manager.backtest_bot(self.ticks)
-            
-            log.info(f"Result ROI: {self.manager.bot_roi()}")
-            self.cache[self.manager.bot_roi()] = (target_percentage, stop_loss)
+                self.manager.backtest_bot(self.ticks)
+                
+                log.info(f"Result ROI: {self.manager.bot_roi()}")
+                self.cache[self.manager.bot_roi()].append(
+                    (target_percentage, stop_loss)
+                )
 
-        res = sorted(list(self.cache.keys()))
-        print(res)
+            self._create_result_bots(top_bots_count)
+
 
     def perm_generator(
             self,
@@ -42,4 +49,30 @@ class ScalperRangeBacktesterApi:
         for i in sample.target_percentage.get_range():
             for j in sample.stop_loss.get_range():
                 yield (round(i, 1), round(j, 1))
+
+    def _create_result_bots(self, top_bots_count: int) -> None:
+        res = sorted(list(self.cache.keys()))
+        log.info(f"Top bots info: {res[:top_bots_count]}")
+
+        def create_bots(res: list, counter: int = 0):
+            for roi in res:
+                if counter == top_bots_count:
+                    return
+                self.manager.edit_interface(
+                    Indicator(),
+                    1,
+                    self.cache[roi][counter // len(res)][0]
+                )
+                self.manager.edit_interface(
+                    Safety(),
+                    2,
+                    self.cache[roi][counter // len(res)][1]
+                )
+
+                self.manager.clone_bot_and_save()
+                counter += 1
+
+            return create_bots(res, counter)
+
+        create_bots(res[:top_bots_count])
 
