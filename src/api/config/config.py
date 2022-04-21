b931fbc68daec3d76353324278d499390bae3889
usrsem <andrey7menov@gmail.com>
2022-04-21 22:33:26 +0300

    Adding bot config path to main config

--- src/api/config/config.py
+++ src/api/config/config.py
@@ -4,6 +4,8 @@ from haasomeapi.dataobjects.custombots.ScalperBot import ScalperBot
 
 toolbox_settings_path = "./toolbox_settings.ini"
 
+bots_config_path = "./bot_configs/{bot_config_name}"
+
 custom_bot_types: dict[int, Type] = dict({
     3: ScalperBot,
     15: MadHatterBot
