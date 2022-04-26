5ee881f57182e2b1ed4e7b0cc4856dd10a1889f9
usrsem <andrey7menov@gmail.com>
2022-04-26 04:03:05 +0300

    Adding new types for GUI

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,6 @@
+import re
 import numpy as np
+from dataclasses import dataclass
 from typing import NamedTuple
 from haasomeapi.dataobjects.custombots.MadHatterBot import MadHatterBot
 from haasomeapi.dataobjects.custombots.ScalperBot import ScalperBot
@@ -38,4 +40,37 @@ class BacktestRange(NamedTuple):
 class SclaperBacktestSample(NamedTuple):
     target_percentage: BacktestRange
     stop_loss: BacktestRange
+
+
+class uri:
+    pattern = re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+    def __init__(self, uri: str) -> None:
+        if not re.search(self.pattern, uri):
+            raise ValueError
+        self.uri = uri
+
+    def __str__(self) -> str:
+        return self.uri
+
+    __repr__ = __str__
+
+
+@dataclass
+class Config:
+    server: uri = uri("127.0.0.1")
+    secret: str = "123"
+    year: int = 2022
+    month: int = 4
+    day: int = 22
+    hour: int = 1
+    min: int = 30
+    target_percentage_start: float = 0.5
+    target_percentage_end: float = 2.0
+    target_percentage_step: float = 0.1
+    stop_loss_start: float = 1.5 
+    stop_loss_end: float = 5.0
+    stop_loss_step: float = 0.2
+    config_backtesting_top_bots_count: int = 5
+    config_backtesting_batch_size: int = 50
+
 
