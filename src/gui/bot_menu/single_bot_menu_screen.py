743924fe3242619de8d029f44b2026b7b45b16b0
usrsem <andrey7menov@gmail.com>
2022-04-30 19:36:08 +0300

    Adding regenerating options to bot menu

--- src/gui/bot_menu/single_bot_menu_screen.py
+++ src/gui/bot_menu/single_bot_menu_screen.py
@@ -6,7 +6,7 @@ from kivy.lang import Builder
 from api.bots.BotManager import BotManager
 from api.bots.mad_hatter.MadHatterBotManager import MadHatterBotManager
 from api.bots.scalper.ScalperBotManager import ScalperBotManager
-from gui.default_widgets import LabelButton
+from gui.default_widgets import LabelButton, ScrollingGridLayout
 
 
 Builder.load_file("./src/gui/bot_menu/single_bot_menu_screen.kv")
@@ -18,6 +18,9 @@ class SingleBotMenuScreen(Screen):
         super().__init__(**kwargs)
 
         self.bot_manager: BotManager
+        self.bot_actions: ScrollingGridLayout = ScrollingGridLayout()
+        self.ids.scroll_view.add_widget(self.bot_actions)
+
         self.main_options: dict[str, Callable] = {
             "Select interface": self.select_interface,
             "Select another bot": self.select_another_bot,
@@ -35,22 +38,28 @@ class SingleBotMenuScreen(Screen):
     def prepare(self, bot_manager: BotManager) -> None:
         print(f"{bot_manager=}")
         self.bot_manager = bot_manager
+        self._clear_buttons()
         self._generate_buttons()
 
+    def _clear_buttons(self) -> None:
+        self.ids.scroll_view.remove_widget(self.bot_actions)
+        self.bot_actions = ScrollingGridLayout()
 
     def _generate_buttons(self) -> None:
         bot_manager_type: Type = type(self.bot_manager)
         additional_options = self.additional_options[bot_manager_type]
 
         for label, action in additional_options.items():
-            self.ids.bot_actions.add_widget(
+            self.bot_actions.add_widget(
                 LabelButton(text=label, on_release=action)
             )
 
         for label, action in self.main_options.items():
-            self.ids.bot_actions.add_widget(
+            self.bot_actions.add_widget(
                 LabelButton(text=label, on_release=action)         
             )
+
+        self.ids.scroll_view.add_widget(self.bot_actions)
 
 
     def select_interface(self, _: LabelButton) -> None:
