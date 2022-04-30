8b3555c4e0ae2a504b618a80e8329f08b4af6019
usrsem <andrey7menov@gmail.com>
2022-04-30 19:06:24 +0300

    Adding ignored options to API config

--- src/api/config.py
+++ src/api/config.py
@@ -1,4 +1,17 @@
+from typing import DefaultDict
+
+
+
 toolbox_settings_path = "./toolbox_settings.ini"
 
 bots_config_path = "./bot_configs/{bot_config_name}Config.json"
 
+ignored_options: DefaultDict[str, DefaultDict[str, tuple[str, ...]]] = \
+    DefaultDict(lambda: DefaultDict(lambda: ()))
+
+ignored_options["MadHatterBot"]["Mad Hatter BBands"] = tuple([
+    "MA Type",
+    "Require FCC",
+    "Reset Middle",
+    "Allow Mid Sells"
+])
