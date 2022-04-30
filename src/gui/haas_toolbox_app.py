76b580c91f54b9c1d75e5aee398f471bf7a67c34
usrsem <andrey7menov@gmail.com>
2022-04-30 18:40:30 +0300

    Adding interface option screen to app

--- src/gui/haas_toolbox_app.py
+++ src/gui/haas_toolbox_app.py
@@ -1,7 +1,11 @@
 from gui.bot_menu.single_bot_menu_screen import SingleBotMenuScreen
 from gui.bot_selector.bot_selector_screen import BotSelectorScreen
-from gui.bot_type_selector.bot_type_selector_screen import BotTypeSelectorScreen
-from gui.interface_selector.interface_selector_screen import InterfaceSelectorScreen
+from gui.bot_type_selector.bot_type_selector_screen \
+        import BotTypeSelectorScreen
+from gui.interface_selector.interface_selector_screen \
+        import InterfaceSelectorScreen
+from gui.interface_option_selector.interface_option_selector_screen \
+        import InterfaceOptionSelectorScreen
 from gui.settings.settings_screen import SettingsScreen
 from gui.menu.menu_screen import MenuScreen
 
@@ -19,6 +23,8 @@ class HaasToolBoxApp(App):
         sm.add_widget(BotSelectorScreen(name="bot_selector"))
         sm.add_widget(InterfaceSelectorScreen(name="interface_selector"))
         sm.add_widget(SingleBotMenuScreen(name="single_bot_menu"))
+        sm.add_widget(InterfaceOptionSelectorScreen(
+            name="interface_option_selector"))
 
         return sm
 
