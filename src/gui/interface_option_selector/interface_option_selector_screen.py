01643cc7735e00c182eff4791d1bddf03dcb5705
usrsem <andrey7menov@gmail.com>
2022-04-30 18:23:06 +0300

    Adding package for intreface option selector screen

---
+++ src/gui/interface_option_selector/interface_option_selector_screen.py
@@ -0,0 +1,11 @@
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+
+Builder.load_file(
+    "./src/gui/interface_option_selector/interface_option_selector_screen.kv")
+
+
+class InterfaceOptionSelectorScreen(Screen):
+    pass
+
