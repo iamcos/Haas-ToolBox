206e386c88a1c485eb0d60c0884a02afe6d12a50
usrsem <andrey7menov@gmail.com>
2022-05-05 01:38:12 +0300

    Adding multiple bot menu

---
+++ src/gui/bot_menu/multiple_bot_menu_screen.py
@@ -0,0 +1,48 @@
+from typing import Type
+from kivy.lang import Builder
+from api.bots.BotManager import BotManager
+from api.factories import bot_managers_factory
+from api.models import Bot
+
+from gui.bot_menu.base_bot_menu_screen import BaseBotMenuScreen
+from api.bots.mad_hatter.MadHatterBotManager import MadHatterBotManager
+from api.bots.scalper.ScalperBotManager import ScalperBotManager
+from gui.default_widgets import LabelButton
+from loguru import logger as log
+
+
+Builder.load_file("./src/gui/bot_menu/multiple_bot_menu_screen.kv")
+
+
+class MultipleBotMenuScreen(BaseBotMenuScreen):
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        self.bots: set[Bot] = set()
+
+        self.add_additional_option(
+            MadHatterBotManager,
+            {"Config backtesting": self.config_backtesting}
+        )
+
+        self.add_additional_option(
+            ScalperBotManager,
+            {"Range backtesting": self.range_backtesting}
+        )
+
+    def prepare(self, bots: set[Bot]) -> None:
+        self.bots.update(bots)
+        bot: Bot = bots.pop()
+        bots.add(bot)
+
+        self.bot_manager = bot_managers_factory.get_bot_manager_by_bot(bot)
+        bot_manager_type: Type[BotManager] = type(self.bot_manager)
+
+        self.generate_buttons(bot_manager_type)
+
+    def config_backtesting(self, _: LabelButton) -> None:
+        log.debug(f"Config backtesting")
+
+    def range_backtesting(self, _: LabelButton) -> None:
+        log.debug(f"Range backtesting")
