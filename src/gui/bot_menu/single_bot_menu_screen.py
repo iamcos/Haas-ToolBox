47305a46dfe446d0648bd7dbd95facea34fa8c8f
usrsem <andrey7menov@gmail.com>
2022-05-05 01:38:02 +0300

    Using new created base bot menu class

--- src/gui/bot_menu/single_bot_menu_screen.py
+++ src/gui/bot_menu/single_bot_menu_screen.py
@@ -1,66 +1,40 @@
-from typing import Callable, Type
-from collections import defaultdict
-from kivy.uix.screenmanager import Screen
+from typing import Type
 from kivy.lang import Builder
 
 from api.bots.BotManager import BotManager
 from api.bots.mad_hatter.MadHatterBotManager import MadHatterBotManager
 from api.bots.scalper.ScalperBotManager import ScalperBotManager
-from gui.default_widgets import LabelButton, ScrollingGridLayout
+from gui.bot_menu.base_bot_menu_screen import BaseBotMenuScreen
+from gui.default_widgets import LabelButton
+from loguru import logger as log
 
 
 Builder.load_file("./src/gui/bot_menu/single_bot_menu_screen.kv")
 
 
-class SingleBotMenuScreen(Screen):
+class SingleBotMenuScreen(BaseBotMenuScreen):
 
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-
         self.bot_manager: BotManager
-        self.bot_actions: ScrollingGridLayout = ScrollingGridLayout()
-        self.ids.scroll_view.add_widget(self.bot_actions)
 
-        self.main_options: dict[str, Callable] = {
-            "Select interface": self.select_interface,
-            "Select another bot": self.select_another_bot,
-        }
+        self.add_main_option("Select interface", self.select_interface)
+        self.add_main_option("Select another bot", self.select_another_bot)
 
-        self.additional_options = defaultdict(dict)
-        self.additional_options[MadHatterBotManager] = {
-            "Config backtesting": self.config_backtesting
-        }
-        self.additional_options[ScalperBotManager] = {
-            "Range backtesting": self.range_backtesting
-        }
-    
+        self.add_additional_option(
+            MadHatterBotManager,
+            {"Config backtesting": self.config_backtesting}
+        )
+
+        self.add_additional_option(
+            ScalperBotManager,
+            {"Range backtesting": self.range_backtesting}
+        )
 
     def prepare(self, bot_manager: BotManager) -> None:
-        print(f"{bot_manager=}")
         self.bot_manager = bot_manager
-        self._clear_buttons()
-        self._generate_buttons()
-
-    def _clear_buttons(self) -> None:
-        self.ids.scroll_view.remove_widget(self.bot_actions)
-        self.bot_actions = ScrollingGridLayout()
-
-    def _generate_buttons(self) -> None:
         bot_manager_type: Type = type(self.bot_manager)
-        additional_options = self.additional_options[bot_manager_type]
-
-        for label, action in additional_options.items():
-            self.bot_actions.add_widget(
-                LabelButton(text=label, on_release=action)
-            )
-
-        for label, action in self.main_options.items():
-            self.bot_actions.add_widget(
-                LabelButton(text=label, on_release=action)         
-            )
-
-        self.ids.scroll_view.add_widget(self.bot_actions)
-
+        self.generate_buttons(bot_manager_type)
 
     def select_interface(self, _: LabelButton) -> None:
         self.manager.get_screen("interface_selector").setup(self.bot_manager)
@@ -70,8 +44,7 @@ class SingleBotMenuScreen(Screen):
         self.manager.current = "bot_selector"
 
     def config_backtesting(self, _: LabelButton) -> None:
-        print(f"Confgi backtesting")
+        log.debug(f"Config backtesting")
 
     def range_backtesting(self, _: LabelButton) -> None:
-        print(f"Range backtesting")
-
+        log.debug(f"Range backtesting")
