43dc7c0490792aa70109d278a81f5328936b80d0
idevtier <idevtier@gmail.com>
2022-07-20 09:44:34 +0200

    Refactoring

--- src/api/providers/bot_api_provider.py
+++ src/api/providers/bot_api_provider.py
@@ -7,19 +7,7 @@ class BotApiProvider(Protocol):
 
     def get_all_bot_interfaces(self, bot_guid: GUID) -> tuple[Interface]: ...
 
-    def get_bot_interfaces_by_type(
-        self,
-        guid_or_bot: GUID | Bot,
-        interface_type: Type[Interface]
-    ) -> tuple[Interface]: ...
-
     def get_refreshed_bot(self, bot_guid: GUID) -> Bot: ...
-
-    def update_bot_interface_option(
-        self,
-        option: InterfaceOption,
-        bot_guid: GUID
-    ) -> None: ...
 
     def get_available_interface_types(self) -> tuple[Type[Interface], ...]: ...
 
@@ -28,4 +16,16 @@ class BotApiProvider(Protocol):
     def delete_bot(self, bot_guid: GUID) -> None: ...
 
     def backtest_bot(self, bot_guid: GUID, ticks: int) -> None: ...
+
+    def get_bot_interfaces_by_type(
+        self,
+        guid_or_bot: GUID | Bot,
+        interface_type: Type[Interface]
+    ) -> tuple[Interface]: ...
+
+    def update_bot_interface_option(
+        self,
+        option: InterfaceOption,
+        bot_guid: GUID
+    ) -> None: ...
 
