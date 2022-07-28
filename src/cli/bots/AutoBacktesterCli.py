5402d8f79a96605b0616c5488a4e05f407f9417c
idevtier <idevtier@gmail.com>
2022-07-28 16:09:00 +0200

    Reformatting AutoBacktesterCli ABC to Protocol

--- src/cli/bots/AutoBacktesterCli.py
+++ src/cli/bots/AutoBacktesterCli.py
@@ -1,19 +1,5 @@
-from __future__ import annotations
-from abc import ABC, abstractmethod
+from typing import Protocol
 
 
-class AutoBacktesterCli(ABC):
-    @classmethod
-    @abstractmethod
-    def with_manager(cls, manager: BotManager) -> AutoBacktesterCli:
-        pass
-
-    @abstractmethod
-    def start(self) -> None:
-        pass
-
-    @staticmethod
-    @abstractmethod
-    def get_name() -> str:
-        pass
-
+class AutoBacktesterCli(Protocol):
+    def start(self) -> None: ...
