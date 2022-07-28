0644c6dc90d81821536159aaf7e258c164f19a2b
idevtier <idevtier@gmail.com>
2022-07-28 16:09:08 +0200

    Refactoring MadHatter cli

--- src/cli/bots/madhatter/MadHatterCli.py
+++ src/cli/bots/madhatter/MadHatterCli.py
@@ -1,14 +1,22 @@
 from haasomeapi.dataobjects.custombots.MadHatterBot import MadHatterBot
 from cli.bots.BotConfigBacktestCli import BotConfigBacktestCli
 from cli.bots.BotCli import BotCli
+from api.loader import main_context
 
 
 class MadHatterCli(BotCli):
     def __init__(self) -> None:
         super().__init__(MadHatterBot)
 
-        # self.add_menu_action(
-        #     "Start backtesting by config",
-        #     tuple([BotConfigBacktestCli(self.manager).start])
-        # )
+        def config_backtest() -> None:
+            return BotConfigBacktestCli(
+                self.bot_guid,
+                self.provider,
+                main_context.config_manager
+            ).start()
+
+        self.add_menu_action(
+            "Start backtesting by config",
+            tuple([config_backtest])
+        )
 
