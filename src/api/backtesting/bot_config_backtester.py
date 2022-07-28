0ec6eb3faa845493be06a7bf762f2013d87c138b
idevtier <idevtier@gmail.com>
2022-07-28 16:08:14 +0200

    Refactoring bot config backtester

--- src/api/backtesting/bot_config_backtester.py
+++ src/api/backtesting/bot_config_backtester.py
@@ -1,15 +1,15 @@
-from collections import defaultdict
-import json
 import threading
-from typing import Generator
 
-from api.exceptions import BotBacktesterException
+from collections import defaultdict
+from typing import Generator, Iterable
+from api.backtesting.bot_editor import BotEditor
+from api.bot_manager import ApiV3BotManager, BotManager
+from api.domain.dtos import BotConfigSetup
+
 from api.providers.bot_api_provider import BotApiProvider
 
 from api.wrappers.interface_wrapper import InterfaceWrapper
-from api.config import bots_config_path
-from api.domain.types import Interface, ROI, GUID
-from api.loader import main_context
+from api.domain.types import Bot, Interface, ROI, GUID, InterfaceOption, OptionValue
 
 from time import monotonic
 
@@ -20,7 +20,7 @@ from api.exceptions import BotException
 """
 Config sample example:
 
-    "0": {                                          # 0 is a sample counter
+    "0": {                                          # 0 is a sample id
         "bot_config": {
             "Interval": 5,
             "Indicator Signal Consensus": false
@@ -40,61 +40,43 @@ Config sample example:
         "Insurance": {
         }
     }
-
 """
+
+ConfigBacktestResult = dict[ROI, set[GUID]]
 
 
 class BotConfigBacktester:
     def __init__(
         self,
         api: BotApiProvider,
-        batch_size: int,
-        top_bots_count: int
+        editor: BotEditor,
+        setup: BotConfigSetup
     ) -> None:
-        self.manager: BotManager = manager
+        self._api = api
+        self._setup: BotConfigSetup = setup
+        self._editor: BotEditor = editor
+        self._manager = self._setup_bot_manager()
 
-        self.batch_size: int = batch_size
-        self.top_bots_count: int = top_bots_count
-        self.config: dict = self._load_config_from_json()
-        self.ticks = main_context.config_manager.read_ticks()
-        self.log = log
-
-    def set_logger(self, custom_logger) -> None:
-        self.log = custom_logger
-
-    def _load_config_from_json(self) -> dict:
-        file_name: str = bots_config_path.format(
-            bot_config_name=self.manager.bot_name())
-        try:
-            with open(file_name, "r") as f:
-                return json.load(f)["tests"]
-        except FileNotFoundError:
-            raise BotBacktesterException(f"File not found in {file_name}")
-
+    def _setup_bot_manager(self) -> BotManager:
+        manager: BotManager = ApiV3BotManager(self._api)
+        manager.set_bot(self._setup.bot_guid)
+        return manager
 
     def start(self) -> None:
         log.info("Starting config backtesting")
-        with self.manager.new_bot():
+        with self._manager.new_bot():
             results: defaultdict[ROI, set[GUID]] = self._process_backtesting()
             self._delete_useless_bots(results)
-
-    def _delete_useless_bots(self, backtest_results: dict[ROI, set[GUID]]) -> None:
-        roi_to_delete = self._get_roi_to_delete(backtest_results)
-
-        for roi in roi_to_delete:
-            for guid in backtest_results[roi]:
-                self.manager.delete_bot(guid)
-
 
     def _process_backtesting(self) -> defaultdict[ROI, set[GUID]]:
         finish: bool = False
         results: defaultdict[ROI, set[GUID]] = defaultdict(lambda: set())
-        results[self.manager.bot_roi()].add(self.manager.bot_guid())
+        results[self._manager.roi()].add(self._manager.guid())
 
         def target():
             for _ in self._generate_backtested_bots():
-                roi: ROI = self.manager.bot_roi()
-                backtested_bot_guid: GUID = self.manager.bot_guid()
+                roi: ROI = self._manager.roi()
+                backtested_bot_guid: GUID = self._manager.guid()
                 results[roi].add(backtested_bot_guid)
 
                 if finish:
@@ -107,27 +89,44 @@ class BotConfigBacktester:
             t.join()
         except (KeyboardInterrupt, BotException):
             finish = True
+            log.warning("Stopping backtesting...")
             t.join()
-            self.log.warning("Stopping backtesting...")
 
         return results
 
+    def _delete_useless_bots(self, result: ConfigBacktestResult ) -> None:
+        roi_to_delete = self._get_roi_to_delete(result)
 
+        for roi in roi_to_delete:
+            for guid in result[roi]:
+                self._api.delete_bot(guid)
 
     def _generate_backtested_bots(self) -> Generator[None, None, None]:
         for sample in self._get_bot_samples():
-            self._reconfigure_bot(sample)
+            modes, interfaces = self._decompose_sample(sample)
+            self._reconfigure_bot(interfaces)
 
             start: float = monotonic()
+            self._api.backtest_bot(self._setup.bot_guid, self._setup.ticks)
+            time: float = monotonic() - start
+            roi: ROI = self._manager.roi()
 
-            self.manager.backtest_bot(self.ticks)
+            log.info(f"ROI: {roi}, Time passed: {time:.2f}s")
 
-            self.log.info(
-                "ROI: {}, Time passed: {:.2f}s".format(
-                    self.manager.bot_roi(), monotonic() - start))
+            clone: Bot = self._api.clone_and_save_bot(self._setup.bot_guid)
+            self._manager.set_bot(clone)
 
-            self.manager.set_bot(self.manager.clone_bot_and_save())
             yield
+
+    def _decompose_sample(self, sample: dict) -> tuple[dict, list[dict]]:
+        modes: dict = sample['modes']
+        interfaces: list[dict] = []
+
+        for key, value in sample.items():
+            if key != "modes":
+                interfaces.append({key: value})
+
+        return (modes, interfaces)
 
     def _delete_all_created_bots(
         self,
@@ -135,12 +134,15 @@ class BotConfigBacktester:
     ) -> None:
         for guids in list(results.values()):
             for guid in guids:
-                self.manager.delete_bot(guid)
-        self.log.debug("Bots deleted")
+                self._api.delete_bot(guid)
+
+        log.info("Bots deleted")
 
 
     def _get_bot_samples(self) -> list[dict]:
-        return [list(i.values())[0] for i in self.config[:self.batch_size]]
+        config = self._setup.config
+        batch_size = self._setup.batch_size
+        return [list(i.values())[0] for i in config[:batch_size]]
 
     def _get_roi_to_delete(
         self,
@@ -151,39 +153,39 @@ class BotConfigBacktester:
 
         rois: list[ROI] = list(backtest_results.keys())
 
-        res: set[ROI] = set(sorted(rois, reverse=True)[self.top_bots_count:])
+        top_bots_count: int = self._setup.top_bots_count
+        res: set[ROI] = set(sorted(rois, reverse=True)[top_bots_count:])
         res.update([i for i in list(backtest_results.keys()) if i <= 0.0])
 
         return list(set(res))
 
 
-    def _reconfigure_bot(self, sample: dict) -> None:
-        for interface_type in self.manager.get_available_interface_types():
-            if interface_type.__name__ in sample:
-                interfaces: dict = sample[interface_type.__name__]
-
-                for interface_name, options in interfaces.items():
-                    interface: Interface = self._get_interface_by_name(
-                        interface_name)
-                    self._edit_options(options, interface)
+    def _reconfigure_bot(self, interfaces: list[dict]) -> None:
+        for interface in interfaces:
+            for interface_name in [*interface]:
+                log.debug(f"{interface_name=}, {interface[interface_name]=}")
+                options = interface[interface_name]
+                self._edit_options(interface_name, options)
 
 
     def _get_interface_by_name(self, interface_name: str) -> Interface:
         return [
             i
-            for i in self.manager.get_all_interfaces()
+            for i in self._api.get_all_bot_interfaces(self._setup.bot_guid)
             if InterfaceWrapper(i).name == interface_name
         ][0]
 
-    def _edit_options(self, options: dict, interface: Interface) -> None:
-        for option_title, value in options.items():
-            option_num: int = self.manager.get_option_num(
-                type(interface), option_title
-            )
-
-            self.manager.edit_interface(
-                interface,
-                option_num,
-                value
-            )
+    def _edit_options(
+        self,
+        interface_type: str,
+        interfaces: dict 
+    ) -> None:
+        for interface_name, options in interfaces.items():
+            for option_name, option_value in options.items():
+                log.debug(f"{option_name=}, {option_value=}")
+                self._editor.edit_option_by_value(
+                    interface_name,
+                    option_name,
+                    option_value
+                )
 
