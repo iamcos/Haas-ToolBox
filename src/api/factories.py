44cd251fd0b85da9a2f8ff9394b7d128d27a5d90
idevtier <idevtier@gmail.com>
2022-08-02 19:00:15 +0200

    Adding new factories

--- src/api/factories.py
+++ src/api/factories.py
@@ -1,10 +1,14 @@
-from typing import Callable, Type
+from typing import Callable, Type, cast
 
 from haasomeapi.dataobjects.custombots.BaseCustomBot import BaseCustomBot
 from haasomeapi.dataobjects.custombots.MadHatterBot import MadHatterBot
 from haasomeapi.dataobjects.custombots.ScalperBot import ScalperBot
 from haasomeapi.dataobjects.tradebot.TradeBot import TradeBot
+from api.backtesting.backtesting_cache import BacktestingCache, SetBacktestingCache
 from api.backtesting.backtesting_strategy import BacktestingStrategy, FloatBacktestingStrategy, IntBacktestingStrategy
+from api.backtesting.bot_backtester import ApiV3BotBacketster, BotBacktester
+from api.backtesting.fine_tune_backtester import FineTuneBacktester
+from api.backtesting.interface_fine_tune_backtester import InterfaceFineTuneBacktester
 from api.loader import main_context
 from api.bot_manager import ApiV3BotManager, BotManager
 
@@ -53,17 +57,38 @@ def get_bot_manager_by_bot(bot: Bot) -> BotManager:
 
 
 def get_backtesting_strategy(value) -> BacktestingStrategy:
-    print(f"Hello from backtesting strategy factory, {value=}")
-
     if type(value) is int or str(value).replace(".0", "").isdigit():
-        print('int')
         return IntBacktestingStrategy()
     elif type(value) is str and "." in value and value.replace(".", "").isdigit():
         return FloatBacktestingStrategy()
     elif type(value) is float:
         return FloatBacktestingStrategy()
-    
 
     raise BacktestingStrategyCreationException(
         f"Strategy for {value} not implemented")
+
+
+def get_bot_backtester(provider: BotApiProvider) -> BotBacktester:
+    cache: BacktestingCache = SetBacktestingCache();
+    return ApiV3BotBacketster(provider, cache, get_backtesting_strategy)
+
+
+def get_fine_tune_backtester(
+    provider_or_backtester: BotApiProvider | BotBacktester
+) -> FineTuneBacktester:
+    if type(provider_or_backtester) is BotApiProvider:
+        provider_or_backtester = get_bot_backtester(provider_or_backtester)
+    else:
+        provider_or_backtester = cast(BotBacktester, provider_or_backtester)
+
+    return FineTuneBacktester(provider_or_backtester)
+
+
+def get_interface_fine_tune_backtester(
+    provider: BotApiProvider
+) -> InterfaceFineTuneBacktester:
+    backtester: BotBacktester = get_bot_backtester(provider)
+    fine_tune_backtester: FineTuneBacktester = get_fine_tune_backtester(
+            backtester)
+    return InterfaceFineTuneBacktester(fine_tune_backtester)
 
