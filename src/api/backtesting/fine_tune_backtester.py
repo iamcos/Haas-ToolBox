ee5602bf587836feb60417a8c43ae6cb4a83eb90
idevtier <idevtier@gmail.com>
2022-08-02 21:41:48 +0200

    Refactoring

--- src/api/backtesting/fine_tune_backtester.py
+++ src/api/backtesting/fine_tune_backtester.py
@@ -1,5 +1,6 @@
 from api.backtesting.bot_backtester import BotBacktester
 from api.domain.dtos import BacktestResult, BacktestSetupInfo
+from api.loader import log
 from dataclasses import dataclass
 
 
@@ -15,11 +16,12 @@ class FineTuneBacktester:
 
         self._bot_backtester.setup(setup)
 
-        for i in range(0, length * 2 + 1):
+        for i in range(0, length * 2):
             if i % 2 == 0:
                 self._bot_backtester.backtest_up()
             else:
                 self._bot_backtester.backtest_down()
 
+        log.debug(f"Executed fine tune with option {setup.option.title}")
         return self._bot_backtester.set_best_result();
 
